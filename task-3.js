d83ce749fea76bf567d685b51590294b1af06cf0
EgorOgichuk <123835482+EgorOgirchuk@users.noreply.github.com>
2024-05-19 15:26:43 +0300

    init

---
+++ task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+      const contentParsed = parseFloat(content.replace('px', ''))
+      const paddingParsed = parseFloat(padding.replace('px', ''))
+      const borderParsed = parseFloat(border.replace('px', ''))
+
+      return contentParsed + 2 * paddingParsed + 2 * borderParsed
+}
+
+console.log(getElementWidth('50px', '8px', '4px')) // 74
+console.log(getElementWidth('60px', '12px', '8.5px')) // 101
+console.log(getElementWidth('200px', '0px', '0px')) // 200
